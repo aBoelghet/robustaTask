27a092a88b32287b4caa69eca77caac920eb6fb3
aBoelghete <aboelghet.ios.dev2gmail.com>
2020-12-10 01:29:32 +0200

    Almostfinish Search

---
+++ Pods/Target Support Files/Pods-RobustaTask/Pods-RobustaTask-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RobustaTask : NSObject
+@end
+@implementation PodsDummy_Pods_RobustaTask
+@end
--- RobustaTask/Controller/HomeViewController.swift
+++ RobustaTask/Controller/HomeViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController{
     
     // MARK: Outlets
     @IBOutlet weak var reposTableView: UITableView!
-    
+    // searchController
+    let searchController = UISearchController(searchResultsController: nil)
     // MARK: Public View Variables
     let dataRequestOject = DataRequests()
     var repositoriesNameArr: [String] = []
@@ -19,65 +20,110 @@ class HomeViewController: UIViewController {
     var ownerImageURLArr: [String] = []
     var ownerNameArr:[String] = []
     var creationDateArr: [String] = []
-        
+    var filteredRepos: [String]!
+    var isSearching = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set this default values for searchController
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        // Add search to table
+        reposTableView.tableHeaderView = searchController.searchBar
+        // SetTableViewColor
+        reposTableView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         // Get repositriesData from Server
-//        dataRequestOject.dataRequest()
-        dataRequestOject.dataRequest(with: "https://api.github.com/repositories", objectType: [Repository].self) { (result: Result) in
-            switch result {
-            case .success(let repos):
-                for repo in repos{
-                    self.repositoriesNameArr.append(repo.name)
-                    self.dataRequestOject.dataRequest(with: repo.url, objectType: RepoOwner.self) { (result: Result) in
-                        switch result {
-                        case .success(let owner):
-                            print(owner)
-                            self.ownerNameArr.append(owner.name)
-                            self.ownerImageURLArr.append(owner.avatarUrl)
-                            self.creationDateArr.append(owner.created_at)
-                            DispatchQueue.main.async {                            self.reposTableView.reloadData()
+        DispatchQueue.global(qos: .background).async {
+            
+            self.dataRequestOject.dataRequest(with: "https://api.github.com/repositories", objectType: [Repository].self) { (result: Result) in
+                switch result {
+                case .success(let repos):
+                    for repo in repos{
+                        self.repositoriesNameArr.append(repo.name)
+                        self.dataRequestOject.dataRequest(with: repo.url, objectType: RepoDetails.self) { (result: Result) in
+                            switch result {
+                            case .success(let owner):
+                                print(owner)
+                                self.ownerNameArr.append(owner.name)
+                                self.ownerImageURLArr.append(owner.avatarUrl)
+                                self.creationDateArr.append(owner.created_at)
+                                DispatchQueue.main.async {                            self.reposTableView.reloadData()
+                                }
+                                print(self.ownerNameArr.count)
+                            case .failure(let error):
+                                print(error)
                             }
-                        case .failure(let error):
-                            print(error)
                         }
                     }
+                    self.filteredRepos = self.repositoriesNameArr
+                case .failure(let error):
+                    print(error)
                 }
-            case .failure(let error):
-                print(error)
             }
         }
-        
     }
 }
 extension HomeViewController: UITableViewDataSource,UITableViewDelegate{
     // MARK: TableView dataSource methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return repositoriesNameArr.count
+        if searchController.isActive && searchController.searchBar.text != nil {
+            return filteredRepos.count
+        } else {
+            return ownerNameArr.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "RepositoryTableViewCell", for: indexPath)as! RepositoryTableViewCell
         
-        cell.repositoryNamebeLabel.text = self.repositoriesNameArr[indexPath.row]
-        cell.repositoryCreationDateLabel.text = self.creationDateArr[indexPath.row]
-        cell.repositoryOwnerNameLabel.text = self.ownerNameArr[indexPath.row]
-        cell.repositoryOwnerImage.image = UIImage(url: URL(string: ownerImageURLArr[indexPath.row]))
-        cell.backgroundView?.addCornerRadius()
-        cell.backgroundColor = #colorLiteral(red: 0.9214808345, green: 0.9216319919, blue: 0.921448648, alpha: 1)
+        
+        if searchController.isActive && searchController.searchBar.text != nil {
+            let index = filteredRepos.startIndex.advanced(by: indexPath.row)
+            cell.repositoryNamebeLabel.text = self.repositoriesNameArr[index]
+            cell.repositoryCreationDateLabel.text = self.creationDateArr[index]
+            cell.repositoryOwnerNameLabel.text = self.ownerNameArr[index]
+            cell.repositoryOwnerImage.image = UIImage(url: URL(string: self.ownerImageURLArr[index]))
+            cell.repositoryOwnerImage.layer.masksToBounds = true
+            cell.repositoryOwnerImage.layer.cornerRadius = (cell.repositoryOwnerImage.layer.frame.width)/2
+            cell.backgroundView?.addCornerRadius()
+            cell.backgroundColor = #colorLiteral(red: 0.9214808345, green: 0.9216319919, blue: 0.921448648, alpha: 1)
+        } else {
+            let index = repositoriesNameArr.startIndex.advanced(by: indexPath.row)
+            cell.repositoryNamebeLabel.text = self.repositoriesNameArr[index]
+            cell.repositoryCreationDateLabel.text = self.creationDateArr[index]
+            cell.repositoryOwnerNameLabel.text = self.ownerNameArr[index]
+            cell.repositoryOwnerImage.image = UIImage(url: URL(string: self.ownerImageURLArr[index]))
+            cell.repositoryOwnerImage.layer.masksToBounds = true
+            cell.repositoryOwnerImage.layer.cornerRadius = (cell.repositoryOwnerImage.layer.frame.width)/2
+            cell.backgroundView?.addCornerRadius()
+            cell.backgroundColor = #colorLiteral(red: 0.9214808345, green: 0.9216319919, blue: 0.921448648, alpha: 1)
+        }
+        
+    
         return cell
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 90
     }
-}
-extension HomeViewController:UISearchBarDelegate{
-    // MARK: SerachBar delegete methods
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        //
-    }
-}
    
+   
+}
+extension HomeViewController:UISearchResultsUpdating{
+  
+    
+    // MARK: SerachController delegete methods
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchText: searchController.searchBar.text!)
+    }
+    func filterContentForSearchText(searchText: String, scope: String = "All") {
+        print("Serach Success")
+    }
+
+}
+    
+
 extension UIView {
     
     func addCornerRadius(width:CGFloat = 3, height:CGFloat = 3, Opacidade:Float = 0.7, radius:CGFloat = 3){
